ec775c58887f69f50763dad07a28e221a1f3740c
Bshy201 <63172060+Bshy201@users.noreply.github.com>
2023-03-27 16:26:04 -0400

    Add files via upload
    Added code to implicitly call toString method and output information about Policy and PolicyHolders

--- Policy.java
+++ Policy.java
@@ -1,35 +1,23 @@
-public class Policy {
+public class Policy{
    
+   private static int instancePolicy;
+   private PolicyHolder holder;
    private int policyNumber;
    private String providerName;
-   private String holderFirstName;
-   private String holderLastName;
-   private int holderAge;
-   private String smokingStatus;
-   private double holderHeight;
-   private double holderWeight;
-   
+      
    public Policy(){
       policyNumber = 0;
       providerName = "";
-      holderFirstName = "";
-      holderLastName = "";
-      holderAge = 0;
-      smokingStatus = "";
-      holderHeight = 0.0;
-      holderWeight = 0.0;
-   }
-   
-   public Policy(int policyNumber, String providerName, String holderFirstName, String holderLastName, int holderAge, String smokingStatus, double holderHeight, double holderWeight){
+      holder = new PolicyHolder();
+      instancePolicy++;
+     }
+        
+   public Policy(int policyNumber, String providerName, PolicyHolder holder){
       this.policyNumber = policyNumber;
       this.providerName = providerName;
-      this.holderFirstName = holderFirstName;
-      this.holderLastName = holderLastName;
-      this.holderAge = holderAge;
-      this.smokingStatus = smokingStatus;
-      this.holderHeight = holderHeight;
-      this.holderWeight = holderWeight;
-   }
+      holder = new PolicyHolder(holder);
+      instancePolicy++;
+        }
    
    /**
    @return the policy number that the user entered
@@ -45,54 +33,15 @@ public class Policy {
     public String getProviderName(){
       return providerName;
    }
-    /**
-   @return the first name for the holder that the user entered
-   */
-
-    public String getHolderFirstName(){
-      return holderFirstName;
-   }
-    /**
-   @return the last name of the holder that the user entered
-   */
-
    
-    public String getHolderLastName(){
-      return holderLastName;
-   }
-    /**
-   @return the age of the holder that the user entered
-   */
-
-    public int getHolderAge(){
-      return holderAge;
-   }
-    /**
-   @return the smoking status of the holder that the user entered
-   */
-
-   
-    public String getSmokingStatus(){
-      return smokingStatus;
-   }
-    /**
-   @return the height of the holder that the user entered
-   */
-
-   
-    public double getHolderHeight(){
-      return holderHeight;
+   public static int getInstancePolicy(){
+      return instancePolicy;
    }
    
-    /**
-   @return the the weight of the holder that the user entered
-   */
-
-   
-    public double getHolderWeight(){
-      return holderWeight;
+   public PolicyHolder getPolicyHolder(){
+      return new PolicyHolder(holder);
    }
-   
+      
     /**
    @param the policy number that represents the holder
    */
@@ -108,90 +57,11 @@ public class Policy {
     public void setProviderName(String providerName){
       this.providerName = providerName;
    }
-    /**
-   @param the first name that represents the holder
-   */
-
    
-    public void setHolderFirstName(String holderFirstName){
-      this.holderFirstName = holderFirstName;
-   }
-    /**
-   @param the last name that represents the holder
-   */
-
-   
-    public void setHolderLastName(String holderLastName){
-      this.holderLastName = holderLastName;
-   }
-    /**
-   @param the age that represents the holder
-   */
-
-    public void setHolderAge(int holderAge){
-      this.holderAge = holderAge;
-   }
-   /**
-   @param the smoking status that represents the holder
-   */
-
-   
-    public void setSmokingStatus(String smokingStatus){
-      this.smokingStatus = smokingStatus;
-   }
-    /**
-   @param the height that represents the holder
-   */
-
-   
-    public void setHolderHeight(double holderHeight){
-      this.holderHeight = holderHeight;
-   }
-   
-    /**
-   @param the weight that represents the holder
-   */
-
-   
-    public void setHolderWeight(double holderWeight){
-      this.holderWeight = holderWeight;
-   }
-    /**
-   @param the height that represents the holder
-   @param the weight that represents the holder
-   @return the BMI that represents the holder
-   */
-
-   
-   public double calcBMI(){
-      double BMI = 0;
-      BMI = (this.holderWeight * 703)/(this.holderHeight * this.holderHeight);
-        return BMI;
-   }
-    /**
-   @param the age that represents the holder
-   @param the smoking status that represents the holder
-   @param the BMI that represents the holder
-   @return the policy fee that the holder would pay
-   */
-
-
-   
-   public double calcPolicyPrice(double BMI){
-      final double BASE_FEE = 600.00;
-      double totalFee = 0.0; 
-      totalFee += BASE_FEE;
-      
-      if (this.holderAge >  50 ){
-         totalFee += 75.00;
-      }
-      if (this.smokingStatus.toLowerCase().charAt(0) == 's'){
-         totalFee += 100.00;
-      }
-      if (BMI > 35) {
-         totalFee += ((BMI - 35) * 20);
-      }
-      
-         return totalFee;
+   @Override       
+   public String toString(){
+      String str = "Policy Number: " + this.policyNumber;
+      String str2 = "\nProvider Name: " + this.providerName;
+      return str + str2;
    }
 }
---
+++ PolicyHolder.java
@@ -0,0 +1,193 @@
+public class PolicyHolder {
+
+   private String holderFirstName;
+   private String holderLastName;
+   private int holderAge;
+   private String smokingStatus;
+   private double holderHeight;
+   private double holderWeight;
+   
+   public PolicyHolder(){
+      holderFirstName = "";
+      holderLastName = "";
+      holderAge = 0;
+      smokingStatus = "";
+      holderHeight = 0.0;
+      holderWeight = 0.0;
+   }
+   
+    public PolicyHolder(String holderFirstName, String holderLastName, int holderAge, String smokingStatus, double holderHeight, double holderWeight){
+      this.holderFirstName = holderFirstName;
+      this.holderLastName = holderLastName;
+      this.holderAge = holderAge;
+      this.smokingStatus = smokingStatus;
+      this.holderHeight = holderHeight;
+      this.holderWeight = holderWeight;
+   }
+   
+   public PolicyHolder(PolicyHolder object2){
+      holderFirstName = object2.holderFirstName;
+      holderLastName = object2.holderLastName;
+      holderAge = object2.holderAge;
+      smokingStatus = object2.smokingStatus;
+      holderHeight = object2.holderHeight;
+      holderWeight = object2.holderWeight;
+   }
+   
+    /**
+   @return the first name for the holder that the user entered
+   */
+
+    public String getHolderFirstName(){
+      return holderFirstName;
+   }
+    /**
+   @return the last name of the holder that the user entered
+   */
+
+   
+    public String getHolderLastName(){
+      return holderLastName;
+   }
+    /**
+   @return the age of the holder that the user entered
+   */
+
+    public int getHolderAge(){
+      return holderAge;
+   }
+    /**
+   @return the smoking status of the holder that the user entered
+   */
+
+   
+    public String getSmokingStatus(){
+      return smokingStatus;
+   }
+    /**
+   @return the height of the holder that the user entered
+   */
+
+   
+    public double getHolderHeight(){
+      return holderHeight;
+   }
+   
+    /**
+   @return the the weight of the holder that the user entered
+   */
+
+   
+    public double getHolderWeight(){
+      return holderWeight;
+   }
+   
+     /**
+   @param the first name that represents the holder
+   */
+
+    public void setHolderFirstName(String holderFirstName){
+      this.holderFirstName = holderFirstName;
+   }
+    /**
+   @param the last name that represents the holder
+   */
+
+   
+    public void setHolderLastName(String holderLastName){
+      this.holderLastName = holderLastName;
+   }
+    /**
+   @param the age that represents the holder
+   */
+
+    public void setHolderAge(int holderAge){
+      this.holderAge = holderAge;
+   }
+   /**
+   @param the smoking status that represents the holder
+   */
+
+   
+    public void setSmokingStatus(String smokingStatus){
+      this.smokingStatus = smokingStatus;
+   }
+    /**
+   @param the height that represents the holder
+   */
+
+   
+    public void setHolderHeight(double holderHeight){
+      this.holderHeight = holderHeight;
+   }
+   
+    /**
+   @param the weight that represents the holder
+   */
+
+   
+    public void setHolderWeight(double holderWeight){
+      this.holderWeight = holderWeight;
+   }
+    /**
+   @param the height that represents the holder
+   @param the weight that represents the holder
+   @return the BMI that represents the holder
+   */
+
+   
+   public double calcBMI(){
+      double BMI = 0;
+      BMI = (this.holderWeight * 703)/(this.holderHeight * this.holderHeight);
+        return BMI;
+   }
+   
+    /**
+   @param the age that represents the holder
+   @param the smoking status that represents the holder
+   @param the BMI that represents the holder
+   @return the policy fee that the holder would pay
+   */
+
+
+   
+   public double calcPolicyPrice(double BMI){
+      final double BASE_FEE = 600.00;
+      double totalFee = 0.0; 
+      totalFee += BASE_FEE;
+      
+      if (this.holderAge >  50 ){
+         totalFee += 75.00;
+      }
+      if (this.smokingStatus.toLowerCase().charAt(0) == 's'){
+         totalFee += 100.00;
+      }
+      if (BMI > 35) {
+         totalFee += ((BMI - 35) * 20);
+      }
+      
+         return totalFee;
+   }
+   
+   
+
+
+   public String toString(){
+   
+   String str = "Policy Holder's First name: " + this.holderFirstName;
+   String str2 = "\nPolicy Holder's Last name: " + this.holderLastName;
+   String str3 = "\nPolicy Holder's Age" + this.holderAge;
+   String str4 = "\nSmoking Status: " + this.smokingStatus;
+   String str5 = "\nPolicy Holder's Weight: " + this.holderWeight;
+   String str6 = "\nPolicy Holder's Height: " + this.holderHeight;
+   String str7 = "\nPolicy Holder's BMI :" + calcBMI();
+   String str8 = String.format("\nPolicy Price:  $%.2f", calcPolicyPrice(calcBMI()));
+   
+   return str + str2 + str3 + str4 + str5 + str6 + str7 + str8;
+
+   }
+   
+   
+   
+
+}
